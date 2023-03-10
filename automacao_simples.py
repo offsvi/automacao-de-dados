8010e901a0e47374cf6f664f0269463656d01a65
Vítor de Oliveira <vitorsk807@gmail.com>
2023-03-09 23:55:15 -0300

    Iniciando código

---
+++ automacao_simples.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Ler o arquivo CSV
+df = pd.read_csv('dados.csv')
+
+# Exibir as primeiras 5 linhas
+print(df.head())
+
+# Calcular o preço médio
+preco_medio = df['preco'].mean()
+print('Preço médio:', preco_medio)
+
+# Salvar os resultados em um novo arquivo CSV
+df.to_csv('resultado.csv', index=False)

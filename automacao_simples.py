725d99054de70c0b58a59900c17320334609f338
Vítor de Oliveira <vitorsk807@gmail.com>
2023-03-10 23:32:31 -0300

    Mudando comentario de fixação

--- automacao_simples.py
+++ automacao_simples.py
@@ -10,5 +10,5 @@ print(df.head())
 preco_medio = df['preco'].mean()
 print('Preço médio:', preco_medio)
 
-# Salvar os resultados em um novo arquivo CSV
+# Salvar os resultados em um novo arquivo CSV 
 df.to_csv('resultado.csv', index=False)

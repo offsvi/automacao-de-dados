38e1e4486fc35a4e62df9ab97c8146ee43c473d1
Vítor de Oliveira <vitorsk807@gmail.com>
2023-03-12 23:15:02 -0300

    Alterando def

--- automacao_simples.py
+++ automacao_simples.py
@@ -8,7 +8,7 @@ print(df.head())
 
 # Calcular o preço médio
 preco_medio = df['preco'].mean()
-print('Preço médio:', preco_medio) 
+print('Preço médio:', preco_medio)
 
 # Salvar os resultados em um novo arquivo CSV 
 df.to_csv('resultado.csv', index=False)
